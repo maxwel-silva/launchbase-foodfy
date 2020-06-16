3344ad4007804946590789d66bd7e597b8fe5c6c
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-06-16 19:56:49 -0300

    first commit

---
+++ desafios/aposentadoria.js
@@ -0,0 +1,39 @@
+// Crie um programa para calcular a aposentadoria de uma pessoa
+
+const pessoa = [
+  {
+    nome: 'Maxwel Silva',
+    idade: 63,
+    sexo: 'M',
+    contribuicao: 35
+  },
+
+  {
+    nome: 'Silvana Pereira',
+    idade: 48,
+    sexo: 'F',
+    contribuicao: 23
+  }
+]
+
+function calcularContribuicao(receber) {
+  return calculo = receber.idade + receber.contribuicao
+}
+
+const aposentadoria = calcularContribuicao(pessoa[1])
+
+function mensagemAposentadoria(nome, idade, sexo, contribuicao, calculo) {
+
+  const aposentadoriaHomem = (sexo == 'M' && contribuicao >= 35 && calculo >= 95)
+  const aposentadoriaMulher = (sexo == 'F' && contribuicao >= 30 && calculo >= 85)
+
+  if (aposentadoriaHomem || aposentadoriaMulher) {
+    console.log(`\n Nome: ${nome}\nIdade: ${idade}\nSexo: ${sexo}\nMensagem: Você pode se aposentar!`)
+  } else {
+    console.log(`\nNome: ${nome}\nIdade: ${idade}\nSexo: ${sexo}\nMensagem: Você não pode se aposentar!`)
+  }
+}
+
+mensagemAposentadoria(pessoa[1].nome, pessoa[1].idade, pessoa[1].sexo)
+
+
--- desafios/imc.js
+++ desafios/imc.js
@@ -1,24 +1,18 @@
+// Crie um programa para calcular o IMC e nível de obesidade de uma pessoa
+
 const pessoas = [
   {
     nome: 'Carlos',
     peso: 40,
     altura: 1.88
-  },
-
-  {
-    nome: 'Vanessa',
-    peso: 122,
-    altura: 1.62
-  },
+  }
 ]
 
 function calcularPeso(pessoa) {
   return (pessoa.peso / (pessoa.altura * pessoa.altura))
- 
 }
 
-const mediaIMC0 = calcularPeso(pessoas[0])
-const mediaIMC1 = calcularPeso(pessoas[1])
+const mediaIMC = calcularPeso(pessoas[0])
 
 function enviarMensagem(pessoa, imc) {
 
@@ -26,9 +20,9 @@ function enviarMensagem(pessoa, imc) {
     console.log(`Nome: ${pessoa} \nIMC: ${imc} \nMensagem: Você está acima do peso`)
     
   } else {
-    console.log(`Nome: ${pessoa} \nIMC: ${imc} \nMensagem: Você não está acima do peso\n`)
+    console.log(`\nNome: ${pessoa} \nIMC: ${imc} \nMensagem: Você não está acima do peso\n`)
   }
 }
 
-enviarMensagem(pessoas[0].nome, mediaIMC0.toFixed(4))
-enviarMensagem(pessoas[1].nome, mediaIMC1.toFixed(4))
+enviarMensagem(pessoas[0].nome, mediaIMC.toFixed(4))
+
