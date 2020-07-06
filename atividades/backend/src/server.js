d76f34aa10f04ad98d8186f2dade1e7f22448755
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-07-05 23:43:14 -0300

    backend commit

--- atividades/backend/src/server.js
+++ atividades/backend/src/server.js
@@ -7,16 +7,19 @@ const server = express()
 
 server.use(express.static('public'))
 
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
   express: server
 })
 
 
-
 server.get('/', function (request, response) {
-  return response.render('index.html')
+  return response.render('about')
+})
+
+server.get('/cursos', function (request, response) {
+  return response.render('cursos')
 })
 
 server.listen(5000)
